b27229ed849511aae1b91826a9aab0e2c387f291
filecage <mail@dbeuchert.com>
2021-01-19 04:41:08 +0100

    implements CurlyBraces parser

---
+++ src/Parser/KeyBodies/CurlyBracesKeyParser.php
@@ -0,0 +1,99 @@
+<?php
+
+    namespace Tholabs\I18nBuild\Parser\KeyBodies;
+
+    use Tholabs\I18nBuild\Exceptions\ParserException;
+    use Tholabs\I18nBuild\Parser\Parser;
+    use Tholabs\I18nBuild\Tokens\KeyDefinition;
+    use Tholabs\I18nBuild\Tokens\Text;
+    use Tholabs\I18nBuild\Tokens\Tokenized;
+    use Tholabs\I18nBuild\Tokens\Variable;
+
+    /**
+     * This parser parses key bodies with variables wrapped in curly braces, e.g.
+     *
+     *      `Hello {name}!`
+     *
+     * will be parsed to
+     *
+     *      Key Token (
+     *          Text Token (Hello ),
+     *          Variable Token (name),
+     *          Text Token (!)
+     *      )
+     */
+    class CurlyBracesKeyParser implements Parser {
+        protected const CURLY_START = '{';
+        protected const CURLY_END = '}';
+
+        function tokenize (?Tokenized $parentToken, string $chunk) : Tokenized {
+            $parentToken = $parentToken ?? new KeyDefinition('');
+
+            // Create a stream and move it to the beginning
+            $chunkStream = fopen('php://temp', 'w+');
+            fputs($chunkStream, $chunk);
+            rewind($chunkStream);
+
+            return new KeyDefinition($parentToken->getKeyName(), ...$parentToken->getBodyTokens(), ...$this->loopSeek($chunkStream, static::CURLY_START, static::CURLY_END));
+        }
+
+        private function loopSeek ($stream, string $needleStart, string $needleEnd) : \Generator {
+            $textBody = $this->seek($stream, $needleStart);
+            if (!empty($textBody)) {
+                yield new Text($textBody);
+            }
+
+            if (!feof($stream)) {
+                $variableName = $this->seek($stream, $needleEnd);
+
+                // If the stream is at its end, check if the ending tag was present or throw an exception if it wasn't
+                if (feof($stream)) {
+                    $needleEndLength = strlen($needleEnd);
+                    fseek($stream, fstat($stream)['size'] - $needleEndLength);
+
+                    if (fread($stream, $needleEndLength) !== $needleEnd) {
+                        throw new ParserException('Expected variable closing tag but encountered EOF in key definition');
+                    }
+                }
+
+                if (!empty($variableName)) {
+                    yield new Variable($variableName);
+                }
+            }
+
+            if (!feof($stream)) {
+                yield from $this->loopSeek($stream, $needleStart, $needleEnd);
+            }
+        }
+
+        /**
+         * Reads the stream until `$needle` is found; leaves the stream at the offset BEFORE `$needle`
+         * Returns the bytes that have been buffered before the encounter
+         *
+         * @param resource $stream
+         * @param string $needle
+         * @return string
+         */
+        private function seek ($stream, string $needle) : string {
+            // `feof()` only matches if we're past EOF, which does not work when resetting the pointer on every iteration
+            // so instead check if we've read the full length
+            $eofOffset = fstat($stream)['size'];
+            $needleByteCount = strlen($needle);
+            $buffer = '';
+
+            while (ftell($stream) !== $eofOffset && fread($stream, $needleByteCount) !== $needle) {
+                // Reset the stream to where it was before we tried to read the starting delimiter
+                fseek($stream, ftell($stream) - $needleByteCount);
+                $buffer .= fread($stream, 1);
+            }
+
+            // If we've read the full size of the stream, move the pointer one step further so that `feof()` returns true
+            if (ftell($stream) === $eofOffset) {
+                fread($stream, 1);
+            }
+
+            return $buffer;
+        }
+
+
+    }
---
+++ tests/Parsers/KeyBodiesParserTest.php
@@ -0,0 +1,118 @@
+<?php
+
+    namespace Tholabs\I18nBuildTests\Parsers;
+
+    use PHPUnit\Framework\TestCase;
+    use Tholabs\I18nBuild\Exceptions\ParserException;
+    use Tholabs\I18nBuild\Parser\KeyBodies\CurlyBracesKeyParser;
+    use Tholabs\I18nBuild\Parser\Parser;
+    use Tholabs\I18nBuild\Tokens\KeyDefinition;
+    use Tholabs\I18nBuild\Tokens\Text;
+    use Tholabs\I18nBuild\Tokens\Variable;
+
+    class KeyBodiesParserTest extends TestCase {
+
+        /**
+         * @param array $expectedKeyBodyTokens
+         * @param string $inputChunk
+         * @param Parser $parser
+         * @throws ParserException
+         * @dataProvider provideSimpleKeysToParse
+         */
+        function testShouldParseSimpleKeyStructs (array $expectedKeyBodyTokens, string $inputChunk, Parser $parser) {
+            /** @var KeyDefinition $token */
+            $token = $parser->tokenize(new KeyDefinition('bodiesParserTest'), $inputChunk);
+
+            $this->assertInstanceOf(KeyDefinition::class, $token);
+            $this->assertSame('bodiesParserTest', $token->getKeyName(), 'KeyBody parser may not change key name');
+
+            $this->assertCount(count($expectedKeyBodyTokens), $token->getBodyTokens());
+            foreach ($expectedKeyBodyTokens as $i => $expectedToken) {
+                $this->assertEquals($expectedToken, $token->getBodyTokens()[$i]);
+            }
+        }
+
+        function provideSimpleKeysToParse () : \Generator {
+            $expectedKeyBodyTokens = [
+                'variable in between' => [
+                    new Text('Hello '),
+                    new Variable('name'),
+                    new Text('!')
+                ],
+                'variable at the start' => [
+                    new Variable('name'),
+                    new Text(', welcome back!')
+                ],
+                'variable at the end' => [
+                    new Text('Hello '),
+                    new Variable('name'),
+                ],
+            ];
+
+            foreach ($this->provideSimpleHelloNameKeyParsers() as $parserName => [$parser, $templates, ]) {
+                $i = 0;
+                foreach ($expectedKeyBodyTokens as $testCaseName => $expectedKeyBodyToken) {
+                    yield $parserName . ', ' . $testCaseName => [$expectedKeyBodyToken, $templates[$i++], $parser];
+                }
+            }
+        }
+
+        function provideSimpleHelloNameKeyParsers () {
+            return [
+                'curly braces parser' => [
+                    new CurlyBracesKeyParser(),
+                    ['Hello {name}!', '{name}, welcome back!', 'Hello {name}'],
+                    ['{name is a good name', 'Hello {name']
+                ]
+            ];
+        }
+
+        /**
+         * @param Parser $parser
+         * @throws ParserException
+         * @dataProvider provideKeyParsers
+         */
+        function testShouldParseKeyWithoutVariable (Parser $parser) {
+            /** @var KeyDefinition $token */
+            $token = $parser->tokenize(new KeyDefinition('bodiesParserTest'), 'Hello World!');
+
+            $this->assertInstanceOf(KeyDefinition::class, $token);
+            $this->assertSame('bodiesParserTest', $token->getKeyName(), 'KeyBody parser may not change key name');
+
+            $expectedKeyBodyTokens = [
+                new Text('Hello World!'),
+            ];
+
+            foreach ($expectedKeyBodyTokens as $i => $expectedToken) {
+                $this->assertEquals($expectedToken, $token->getBodyTokens()[$i]);
+            }
+        }
+
+        function provideKeyParsers () {
+            foreach ($this->provideSimpleHelloNameKeyParsers() as $description => [$parser]) {
+                yield $description => [$parser];
+            }
+        }
+
+        /**
+         * @param Parser $parser
+         * @param string $brokenTemplate
+         * @throws ParserException
+         * @dataProvider provideBrokenVariableDefinitions
+         */
+        function testShouldThrowExceptionForBrokenVariableDefinitions (Parser $parser, string $brokenTemplate) {
+            $this->expectException(ParserException::class);
+            $this->expectExceptionMessage('Parser exception: Expected variable closing tag but encountered EOF in key definition');
+            
+            $parser->tokenize(null, $brokenTemplate);
+        }
+
+        function provideBrokenVariableDefinitions () {
+            foreach ($this->provideSimpleHelloNameKeyParsers() as $parserName => [$parser, $templates, $brokenTemplates]) {
+                foreach ($brokenTemplates as $name => $brokenTemplate) {
+                     yield $parserName . ', ' . $name => [$parser, $brokenTemplate];
+                }
+            }
+        }
+
+    }
