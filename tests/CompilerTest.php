159f1b715209d6d87df2127135acc3e980edf1f2
filecage <mail@dbeuchert.com>
2021-01-19 02:41:15 +0100

    adds Compiler tests

---
+++ tests/CompilerTest.php
@@ -0,0 +1,77 @@
+<?php
+
+    // Overwriting the date function so we always get the same result from our generated code
+    namespace Tholabs\I18nBuild\Writer\Nodes;
+    function date($format) {
+        return \date($format, 1629632262);
+    }
+
+    namespace Tholabs\I18nBuildTests;
+
+    use PHPUnit\Framework\TestCase;
+    use Tholabs\I18nBuild\Compiler;
+    use Tholabs\I18nBuild\Exceptions\UnknownTokenException;
+    use Tholabs\I18nBuild\Tokens\KeyDefinition;
+    use Tholabs\I18nBuild\Tokens\PackageDefinition;
+    use Tholabs\I18nBuild\Tokens\Text;
+    use Tholabs\I18nBuild\Tokens\Tokenized;
+    use Tholabs\I18nBuild\Tokens\Variable;
+    use Tholabs\I18nBuild\Writer\NodeWriters;
+
+    class CompilerTest extends TestCase {
+
+        /** @var resource */
+        private $stream;
+        private Compiler $compiler;
+
+        function setUp () : void {
+            $this->stream = fopen('php://temp', 'w+');
+            $this->compiler = new Compiler($this->stream, ...NodeWriters::getDefault());
+        }
+
+        function testShouldCompilePackage () {
+            $this->compiler->compileToken(new PackageDefinition('foo',
+                new KeyDefinition('bar', new Text('I am a test key')),
+                new KeyDefinition('hello.world', new Text('Hello '), new Variable('name'), new Text('!')),
+            ));
+
+            $this->assertSame(<<<'ExpectedRendering'
+                              <?php
+                              /**
+                               * This file has been automatically created using
+                               * https://github.com/filecage/i18n-build
+                               *
+                               * You should not modify it.
+                               *
+                               * @i18n-package foo
+                               * @i18n-version 1.0.0
+                               * @createdAt 2021-08-22T13:37:42+02:00
+                               */
+                              
+                              return [
+                                  'bar' => fn(array $context) => "I am a test key",
+                                  'hello.world' => fn(array $context) => "Hello {$context['name']}!",
+                              ];
+                              
+                              ExpectedRendering
+            , stream_get_contents($this->stream, -1, 0));
+        }
+
+        function testExpectsCompilerToThrowUnknownTokenException () {
+            $this->expectException(UnknownTokenException::class);
+            $this->expectExceptionMessage('Compiler exception: Unknown token can not be compiled: Tholabs\I18nBuildTests\FooToken');
+
+            $this->compiler->compileToken(new FooToken());
+        }
+
+        function testExpectsCompilerToThrowInvalidArgumentExceptionForInvalidStreams () {
+            $this->expectException(\InvalidArgumentException::class);
+            $this->expectExceptionMessage('Given `$stream` must be a writable resource');
+
+            /** @noinspection PhpParamsInspection */
+            new Compiler('foo');
+        }
+
+    }
+
+    class FooToken implements Tokenized {}
