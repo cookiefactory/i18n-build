749b5ead1878c97fa5ccffbe61f123e1da492867
filecage <mail@dbeuchert.com>
2021-02-01 17:49:49 +0100

    use SeekableString instead of accessing streams directly

--- src/Parser/KeyBodies/CurlyBracesKeyParser.php
+++ src/Parser/KeyBodies/CurlyBracesKeyParser.php
@@ -4,6 +4,7 @@
 
     use Tholabs\I18nBuild\Exceptions\ParserException;
     use Tholabs\I18nBuild\Parser\Parser;
+    use Tholabs\I18nBuild\Parser\SeekableString;
     use Tholabs\I18nBuild\Tokens\KeyDefinition;
     use Tholabs\I18nBuild\Tokens\Text;
     use Tholabs\I18nBuild\Tokens\Tokenized;
@@ -33,85 +34,43 @@
                 throw new ParserException('Input chunk is not part of parent token original chunk');
             }
 
-            // Create a stream and move it to the beginning
-            $chunkStream = fopen('php://temp', 'w+');
-            fputs($chunkStream, $chunk);
-            rewind($chunkStream);
-
             try {
-                $tokens = iterator_to_array($this->loopSeek($chunkStream, static::CURLY_START, static::CURLY_END), false);
+                $tokens = iterator_to_array($this->loopSeek(new SeekableString($chunk), static::CURLY_START, static::CURLY_END), false);
             } catch (ParserException $exception) {
                 throw new ParserException("{$exception->getInnerMessage()} for key `{$parentToken->getKeyName()}`", $exception->getCode(), $exception);
-            } finally {
-                fclose($chunkStream);
             }
 
             return new KeyDefinition($parentToken->getKeyName(), $parentToken->getOriginalChunk(), ...$parentToken->getBodyTokens(), ...$tokens);
         }
 
         /**
-         * @param $stream
+         * @param SeekableString $seeker
          * @param string $needleStart
          * @param string $needleEnd
          * @return \Generator|Tokenized[]
          * @throws ParserException
          */
-        private function loopSeek ($stream, string $needleStart, string $needleEnd) : \Generator {
-            $textBody = $this->seek($stream, $needleStart);
+        private function loopSeek (SeekableString $seeker, string $needleStart, string $needleEnd) : \Generator {
+            $textBody = $seeker->seek($needleStart);
             if (!empty($textBody)) {
                 yield new Text($textBody);
             }
 
-            if (!feof($stream)) {
-                $variableName = $this->seek($stream, $needleEnd);
-
-                // If the stream is at its end, check if the ending tag was present or throw an exception if it wasn't
-                if (feof($stream)) {
-                    $needleEndLength = strlen($needleEnd);
-                    fseek($stream, fstat($stream)['size'] - $needleEndLength);
-
-                    if (fread($stream, $needleEndLength) !== $needleEnd) {
-                        throw new ParserException('Expected variable closing tag but encountered EOF instead in key definition');
-                    }
-                }
+            if (!$seeker->eof()) {
+                $variableName = $seeker->seek($needleEnd, SeekableString::SEEK_ASSERT_TOUCH);
 
                 if (!empty($variableName)) {
                     yield new Variable($variableName);
+                } elseif ($seeker->eof()) {
+                    // If we're at EOF and have no variable name, the closing tag is missing
+                    throw new ParserException('Expected variable closing tag but encountered EOF instead in key definition');
                 }
             }
 
-            if (!feof($stream)) {
-                yield from $this->loopSeek($stream, $needleStart, $needleEnd);
+            // If there is still contents to be consumed, move forward
+            if (!$seeker->eof()) {
+                yield from $this->loopSeek($seeker, $needleStart, $needleEnd);
             }
-        }
-
-        /**
-         * Reads the stream until `$needle` is found; leaves the stream at the offset BEFORE `$needle`
-         * Returns the bytes that have been buffered before the encounter
-         *
-         * @param resource $stream
-         * @param string $needle
-         * @return string
-         */
-        private function seek ($stream, string $needle) : string {
-            // `feof()` only matches if we're past EOF, which does not work when resetting the pointer on every iteration
-            // so instead check if we've read the full length
-            $eofOffset = fstat($stream)['size'];
-            $needleByteCount = strlen($needle);
-            $buffer = '';
-
-            while (ftell($stream) !== $eofOffset && fread($stream, $needleByteCount) !== $needle) {
-                // Reset the stream to where it was before we tried to read the starting delimiter
-                fseek($stream, ftell($stream) - $needleByteCount);
-                $buffer .= fread($stream, 1);
-            }
-
-            // If we've read the full size of the stream, move the pointer one step further so that `feof()` returns true
-            if (ftell($stream) === $eofOffset) {
-                fread($stream, 1);
-            }
-
-            return $buffer;
         }
 
 
